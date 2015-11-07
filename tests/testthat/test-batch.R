13c96c4d27c935393a04d9b00c807993eb9161e4
Gaurav Sood <gsood07@gmail.com>
2015-11-06 20:09:52 -0500

    tests ok, builds ok

--- tests/testthat/test-batch.R
+++
@@ -1,9 +0,0 @@
-context("Create Batch")
-
-test_that("batch creation happens successfully", {
-  skip_on_cran()
-  token <- readRDS("token_file.rds")
-  set_token(token)
-  batch <- create_batch("new_batch")
-  expect_that(batch, is_a("list"))
-})
