771507518878bc7afb80e29fe68667b4d50b9fe6
Gaurav Sood <gsood07@gmail.com>
2015-11-10 18:48:19 -0500

    fixed link

--- R/clarifai.R
+++ R/clarifai.R
@@ -4,7 +4,7 @@
 #' @aliases clarifai
 #'
 #' @description Get Description of Images. Understand what is in the images. See \url{http://clarifai.com/}. 
-#' Details about results of calls to the API can be found at \url{https://api.clarifai.com}.
+#' Details about results of calls to the API can be found at \url{https://developer.clarifai.com}.
 #'
 #' Your need credentials to use this application. 
 #' If you haven't already, you can get this at \url{https://developer.clarifai.com/}.
